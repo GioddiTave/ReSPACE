79055948bfdc20c3fe84ecf786b615773ca7786f
GioddiTave <theej2000@gmail.com>
2024-05-30 15:24:23 +0200

    Scroll is working. newest adaptation for Mobile

--- script.js
+++ script.js
@@ -1,38 +1,19 @@
-const slider = document.querySelector('.slider');
-const sections = gsap.utils.toArray('.slider section'); // Korrigiere den Tippfehler von "untils" zu "utils"
+document.addEventListener('wheel', (e) => {
+    e.preventDefault();
+    document.querySelector('.container').scrollLeft += e.deltaY;
+}, { passive: false });
 
-// Konfiguration des GSAP Timelines mit ScrollTrigger
-let tl = gsap.timeline({
-    defaults: {
-        ease: "none"
-    },
-    scrollTrigger: {
-        trigger: slider,
-        pin: true,
-        scrub: 2,
-        end: () => "+=" + slider.offsetWidth
-    }
-});
+document.addEventListener('touchstart', (e) => {
+    window.startTouch = e.touches[0].clientY;
+}, { passive: false });
 
-// Bewegen des Sliders um 308% des Viewports nach links
-tl.to(slider, {
-    xPercent: -308
-});
+document.addEventListener('touchmove', (e) => {
+    e.preventDefault();
+    let deltaY = window.startTouch - e.touches[0].clientY;
+    document.querySelector('.container').scrollLeft += deltaY;
+    window.startTouch = e.touches[0].clientY;
+}, { passive: false });
 
-// Füge das Mausrad-Scroll-Event hinzu, um horizontal zu scrollen
-slider.addEventListener('wheel', function(event) {
-    event.preventDefault(); // Verhindert das vertikale Scrollen
-    const scrollAmount = 100; // Definiere, wie weit jeder Scrollvorgang gehen soll
-
-    if (event.deltaY > 0) {
-        gsap.to(slider, {
-            x: () => `+=${scrollAmount}`, // Bewegt den Slider horizontal nach rechts
-            ease: "none"
-        });
-    } else {
-        gsap.to(slider, {
-            x: () => `-=${scrollAmount}`, // Bewegt den Slider horizontal nach links
-            ease: "none"
-        });
-    }
-});
+document.addEventListener('touchend', () => {
+    window.startTouch = null;
+}, { passive: false });
